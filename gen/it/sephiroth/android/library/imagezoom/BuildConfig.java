97a8ca9ea81d1ddb7034ad3b63931348fa87c593
Ojas <ojas@weboniselab.com>
2013-08-19 14:58:10 +0530

    library code

---
+++ gen/it/sephiroth/android/library/imagezoom/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package it.sephiroth.android.library.imagezoom;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/imagezoom/graphics/IBitmapDrawable.java
@@ -0,0 +1,15 @@
+package com.imagezoom.graphics;
+
+import com.imagezoom.ImageViewTouchBase;
+
+import android.graphics.Bitmap;
+
+/**
+ * Base interface used in the {@link ImageViewTouchBase} view
+ * @author alessandro
+ *
+ */
+public interface IBitmapDrawable {
+
+	Bitmap getBitmap();
+}
